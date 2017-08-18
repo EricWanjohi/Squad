50b61b572ae4aa85601a4758fb1bc2ec2518dc1b
Eric Wanjohi <ericwanjohi.ew@gmail.com>
2017-08-18 12:32:59 +0300

    Created new routes in App.java, Added hero.vtl form to collect hero attributes, saved hero attributes into session for later use, added newHero.vtl to display hero attributes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,33 @@ public class App{
 		staticFileLocation("/public");
 		String layout = "templates/layout.vtl";
 
-		// Squad Routes... 
+		// Squad Routes...
+		get("/", (request, response) -> {
+			Map<String, Object> model = new HashMap<String, Object>();
+			model.put("template", "templates/hero.vtl");
+			return new ModelAndView(model, layout);
+		}, new VelocityTemplateEngine()); 
+
+		post("/newHero", (request, response) -> {
+			Map<String, Object> model = new HashMap<String, Object>(); // Creates a new Hashmap Object to store inputs...
+			// Creates String variables to store name attribute inputs from hero.vtl
+			String inputtedHeroName = request.queryParams("name");
+			String inputtedHeroAge = request.queryParams("age");
+			String inputtedHeroPowers = request.queryParams("powers");
+			String inputtedHeroWeaknesses = request.queryParams("weaknesses");
+			// Requests session to store values into our String variables from the name attribute in the hero.vtl template...
+			request.session().attribute("name", inputtedHeroName);
+			request.session().attribute("age", inputtedHeroAge);
+			request.session().attribute("powers", inputtedHeroPowers);
+			request.session().attribute("weaknesses", inputtedHeroWeaknesses);
+			// Adds the data to our HashMap object named model above...
+			model.put("name", inputtedHeroName);
+			model.put("age", inputtedHeroAge);
+			model.put("powers", inputtedHeroPowers);
+			model.put("weaknesses", inputtedHeroWeaknesses);
+
+			model.put("template", "templates/newHero.vtl");
+			return new ModelAndView(model, layout);
+		}, new VelocityTemplateEngine());
 	}
 }
